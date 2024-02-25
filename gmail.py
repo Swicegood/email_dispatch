72caaabfdcad2601384820765dc51684ae95f802
Jagadguru <jguru108@gmail.com>
2024-02-25 17:05:25 -0500

    change to command line auth

--- gmail.py
+++ gmail.py
@@ -1,37 +1,12 @@
-import threading
-import webbrowser
-import os
-from http.server import HTTPServer
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.exceptions import RefreshError
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google.auth.exceptions import RefreshError
-import socket
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import os
 
-
-# If modifying these scopes, delete the file token.json.
+# If modifying these SCOPES, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-
-def run_flow_with_timeout(flow, timeout=60):
-    """Run the flow's local server with a timeout."""
-    def run_server():
-        flow.run_local_server(port=0)
-
-    server_thread = threading.Thread(target=run_server)
-    server_thread.daemon = True  # Allow thread to be interrupted
-    server_thread.start()
-    server_thread.join(timeout=timeout)
-
-    if not server_thread.is_alive():
-        # Server thread completed within timeout
-        if flow.credentials and flow.credentials.valid:
-            return flow.credentials
-        else:
-            raise ValueError("Failed to obtain valid credentials.")
-    else:
-        # Timeout reached, server thread still alive (user did not authenticate)
-        raise TimeoutError("Authentication process did not complete within the allotted time.")
 
 def grab_emails(search_str):
     """Shows basic usage of the Gmail API.
@@ -51,18 +26,10 @@ def grab_emails(search_str):
             except RefreshError:
                 os.remove('token.json')  # Remove the invalid token
                 flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-                try:
-                    creds = run_flow_with_timeout(flow, timeout=30)  # 60 seconds timeout
-                except (TimeoutError, ValueError) as e:
-                    print(e)
-                    return []  # or handle the exception as needed
+                creds = flow.run_console()
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            try:
-                creds = run_flow_with_timeout(flow, timeout=30)  # 60 seconds timeout
-            except (TimeoutError, ValueError) as e:
-                print(e)
-                return []  # or handle the exception as needed
+            creds = flow.run_console()
 
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
